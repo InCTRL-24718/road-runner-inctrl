208fd50fade9ed27b2820dfe54ebcce874212643
InCTRL-24718 <ftc24718.inctrl@gmail.com>
2025-02-09 21:40:43 +0000

    Android Studio OpenCV 6.5
    Amended FirstAprilTags.java to add April Tag pose and meta data to telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processors/opmodes/FirstAprilTags.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processors/opmodes/FirstAprilTags.java
@@ -15,7 +15,8 @@ public class FirstAprilTags extends OpMode {
 
     private AprilTagProcessor aprilTagProcessor;
     private VisionPortal visionPortal;
-
+    private String aprilTagName;
+    private int lastAprilTagIdCode;
     private AprilTagDetection aprilTagDetection;
 
     @Override
@@ -28,13 +29,15 @@ public class FirstAprilTags extends OpMode {
 
     @Override
     public void init_loop() {
-        /*int aprilTagIdCode = aprilTagDetection.id;
+
+        /* //Will only detect one April Tag
+        int aprilTagIdCode = aprilTagDetection.id;
         telemetry.addData("April Tag ID Code", aprilTagIdCode);
         telemetry.update();
         */
 
         List<AprilTagDetection> aprilTagDetections;  // list of all detections
-        int aprilTagIdCode;                           // ID code of current detection, in for() loop
+        int aprilTagIdCode = 0;                      // ID code of current detection, in for() loop
 
         // Get a list of AprilTag detections.
         aprilTagDetections = aprilTagProcessor.getDetections();
@@ -44,9 +47,31 @@ public class FirstAprilTags extends OpMode {
 
             if (aprilTagDetection.metadata != null) {  // This check for non-null Metadata is not needed for reading only ID code.
                 aprilTagIdCode = aprilTagDetection.id;
+                aprilTagName = aprilTagDetection.metadata.name;
+                double tagPoseX = aprilTagDetection.ftcPose.x;
+                double tagPoseY = aprilTagDetection.ftcPose.y;
+                double tagPoseZ = aprilTagDetection.ftcPose.z;
+                double tagPosePitch = aprilTagDetection.ftcPose.pitch;
+                double tagPoseRoll = aprilTagDetection.ftcPose.roll;
+                double tagPoseYaw = aprilTagDetection.ftcPose.yaw;
+                double tagPoseRange = aprilTagDetection.ftcPose.range;
+                double tagPoseBearing = aprilTagDetection.ftcPose.bearing;
+                double tagPoseElevation = aprilTagDetection.ftcPose.elevation;
+
+                telemetry.addData("April Tag ID Code", aprilTagIdCode);
+                telemetry.addData("April Tag Name", aprilTagName);
+                telemetry.addData("Tag Pose X", tagPoseX);
+                telemetry.addData("Tag Pose Y", tagPoseY);
+                telemetry.addData("Tag Pose Z", tagPoseZ);
+                telemetry.addData("Tag Pose Pitch", tagPosePitch);
+                telemetry.addData("Tag Pose Roll", tagPoseRoll);
+                telemetry.addData("Tag Pose Yaw", tagPoseYaw);
+                telemetry.addData("Tag Pose Range", tagPoseRange);
+                telemetry.addData("Tag Pose Bearing", tagPoseBearing);
+                telemetry.addData("Tag Pose Elevation", tagPoseElevation);
+                telemetry.update();
 
                 // Now take action based on this tag's ID code, or store info for later action.
-
             }
         }
     }
